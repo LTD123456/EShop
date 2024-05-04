03210256b65c11e90ddec5c16d76a59c5a6e4b5b
Duc Le <Duc.Le@EVCargoCo.onmicrosoft.com>
2024-05-04 14:41:39 +0700

    Handle Success Response Data

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,15 @@
 "use strict";
 
+const { OK,CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(201).json(await AccessService.signUp(req.body));
+
+    new CREATED({
+        message:"Register successfully!!!!",
+        metadata: await AccessService.signUp(req.body),
+    }).send(res);
   };
 }
 
---
+++ src/core/success.response.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const StatusCode = {
+  OK: 200,
+  CREATED: 201,
+};
+
+const ReasonPhrase = {
+  OK: "Success",
+  CREATED: "Created success",
+};
+
+class SuccessResponse {
+  constructor({
+    message,
+    statusCode = StatusCode.OK,
+    reasonStatusCode = ReasonPhrase.OK,
+    metadata = {},
+  }) {
+    this.message = !message ? reasonStatusCode : message;
+    this.status = statusCode;
+    this.metadata = metadata;
+  }
+
+  send(res, header = {}) {
+    return res.status(this.status).json(this);
+  }
+}
+
+class OK extends SuccessResponse {
+  constructor({ message, metadata }) {
+    super({ message, metadata });
+  }
+}
+
+class CREATED extends SuccessResponse {
+  constructor({
+    message,
+    statusCode = StatusCode.CREATED,
+    reasonStatusCode = ReasonPhrase.CREATED,
+    metadata,
+  }) {
+    super({ message, statusCode, reasonStatusCode, metadata });
+  }
+}
+
+module.exports = {OK, CREATED}
